33cdb75edfbd02db1f27ca8813aac0060dabaeba
GHOST <48442414+DanielsKraus@users.noreply.github.com>
2019-10-20 00:57:57 -0400

    Add files via upload

---
+++ smart_trash_can_v4.ino
@@ -0,0 +1,82 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "fauxmoESP.h"
+ 
+#define WIFI_SSID "yournetwork"
+#define WIFI_PASS "yourpassword"
+#define SERIAL_BAUDRATE 115200
+
+#define open_btn 1
+#define close_btn 2
+ 
+fauxmoESP fauxmo;
+ 
+// -----------------------------------------------------------------------------
+// Wifi
+// -----------------------------------------------------------------------------
+void open_trash()
+{
+  Serial.println("OPEN");
+  digitalWrite(open_btn,HIGH);
+  delay(10);
+  digitalWrite(open_btn,LOW);
+}
+void close_trash()
+{
+  Serial.println("CLOSE");
+  digitalWrite(close_btn,HIGH);
+  delay(10);
+  digitalWrite(close_btn,LOW);
+}
+void wifiSetup() {
+ 
+    // Set WIFI module to STA mode
+    WiFi.mode(WIFI_STA);
+ 
+    // Connect
+    Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+ 
+    // Wait
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(100);
+    }
+    Serial.println();
+ 
+    // Connected!
+    Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+}
+ 
+void setup() {
+  // Init serial port and clean garbage
+  Serial.begin(SERIAL_BAUDRATE);
+  pinMode(open_btn, OUTPUT);
+  pinMode(close_btn, OUTPUT);
+  // Ask Alexa
+  Serial.println("ask Alexa, to turn on trash can or off");
+ 
+  // wifi
+  wifiSetup();
+ 
+  // Fauxmo
+  fauxmo.addDevice("trash can");
+  // Gen3 Devices or above
+  fauxmo.setPort(80);
+ 
+  // Allow the FauxMo to be discovered
+  fauxmo.enable(true);
+  
+  fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
+    Serial.print("Device: ");Serial.print(device_name);
+    Serial.print(" state");
+    if(state)
+      open_trash();
+    else
+      close_trash();
+  }); 
+}
+ 
+void loop() {
+  fauxmo.handle();
+}
